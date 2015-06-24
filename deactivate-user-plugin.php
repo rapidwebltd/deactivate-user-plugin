5f2b611c7b6de014d32f377020596dda506a9869
DivineOmega <jordan@hall05.co.uk>
2015-06-24 22:41:14 +0100

    Minor code styling improvements

--- deactivate-user-plugin.php
+++ deactivate-user-plugin.php
@@ -15,35 +15,33 @@ add_action('admin_menu', 'jhdup_deactivate_my_account_menu' );
 
 add_shortcode('jhdup_deactivate_current_user_shortcode', 'jhdup_deactivate_current_user_shortcode_handler' );
 
-function jhdup_deactivate_my_account_menu() {
-    
+function jhdup_deactivate_my_account_menu()
+{
     global $jhdup_page_slug;
     
 	add_menu_page('Deactivate my account', 'Deactivate my account', 'read', $jhdup_page_slug, 'jhdup_deactivate_my_account');
 }
 
-function jhdup_deactivate_my_account() {
-    
+function jhdup_deactivate_my_account() 
+{
 	global $jhdup_page_slug;
 	
-	if (isset($_GET['confirm']))
-	{
+	if (isset($_GET['confirm'])) {
         jhdup_perform_deactivation_of_current_user();
         
         echo '<div class="wrap">';
-    	echo '<h1>Deactivate my account</h1>';
-    	echo '<p>Your account has been deactivated and you have been logged out.</p>';
-    	echo '<p><a href="/">Return to the homepage</a></p>';
-    	echo '</div>';
+        echo '<h1>Deactivate my account</h1>';
+        echo '<p>Your account has been deactivated and you have been logged out.</p>';
+        echo '<p><a href="/">Return to the homepage</a></p>';
+        echo '</div>';
 	}
-	else
-	{
-    	echo '<div class="wrap">';
-    	echo '<h1>Deactivate my account</h1>';
-    	echo '<p>Are you sure you want to delete your account?</p>';
-    	echo '<p><a href="?page='.$jhdup_page_slug.'&confirm">Yes, deactivate my account</p>';
-    	echo '<p><a href="./">No, return to the dashboard</p>';
-    	echo '</div>';
+	else {
+        echo '<div class="wrap">';
+        echo '<h1>Deactivate my account</h1>';
+        echo '<p>Are you sure you want to delete your account?</p>';
+        echo '<p><a href="?page='.$jhdup_page_slug.'&confirm">Yes, deactivate my account</p>';
+        echo '<p><a href="./">No, return to the dashboard</p>';
+        echo '</div>';
 	}
 }
 
